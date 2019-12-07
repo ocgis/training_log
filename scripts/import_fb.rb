7689348a77d61bb225096cac684f9a6dad6c90f4
Christer Gustavsson <cg@nocrew.org>
2019-12-07 18:16:31 +0100

    Added creating of person.

--- scripts/import_fb.rb
+++ scripts/import_fb.rb
@@ -3,23 +3,33 @@ puts "FP importer started"
 def import_fb_user(path)
   puts "About to parse " + path
   doc = Nokogiri::HTML(open(path))
-  puts doc
   
   main_content = doc.xpath('//h1[@id="ctl00_ctl00_MainContentPlaceHolder_Header"]')
   if main_content.size != 1
-    raise "Could not get user name"
+    raise "Could not get person name"
   end
-  user_name = main_content[0].content
+  person_name = main_content[0].content
 
   avatar_tag = doc.xpath('//img[@id="ctl00_ctl00_MainContentPlaceHolder_PersonNavigation1_PhotoImage"]')
-  puts avatar_tag
-  puts avatar_tag.size
-  puts avatar_tag[0]['src']
-  puts avatar_tag[0].attributes
+  if avatar_tag.size != 1
+    raise "Could not get person id"
+  end
   user_match = /\/(\d+)\./.match(avatar_tag[0]['src'])
-  user_id = user_match[1].to_i
-  puts user_id
-  puts user_name
+  person_id = user_match[1].to_i
+  person_objs = Person.where({alt_id: person_id})
+  if person_objs.length == 1
+    person_obj = person_objs[0]
+    person_obj.update({name: person_name})
+    person_obj.save
+    puts "Updated person %d with name %s and alt id %s" % [person_obj.id, person_name, person_id]
+  elsif person_objs.length == 0
+    person_obj = Person.new({name: person_name,
+                             alt_id: person_id})
+    person_obj.save
+    puts "Created person %d with name %s and alt id %s" % [person_obj.id, person_name, person_id]
+  else
+    raise "Found several persons with id"
+  end
 end
 
 ARGV.each do |path|
