f8d2105411d5c44c9b4306345507ffd3f7ad1a3b
Christer Gustavsson <cg@nocrew.org>
2019-12-08 09:55:30 +0100

    Started importing trainings.

--- scripts/import_fb.rb
+++ scripts/import_fb.rb
@@ -1,4 +1,78 @@
+# coding: utf-8
+require 'pp'
+
 puts "FP importer started"
+
+TRANSLATE_TP = {'Runda:' => 'route_id',
+                'Tempo:' => 'pace',
+                'Hastighet:' => 'speed',
+                'Sträcka:' => 'distance'}
+
+def import_fb_training(person_id, path)
+  puts "About to parse training from " + path
+  doc = Nokogiri::HTML(open(path))
+
+  training = {}
+  route_id = nil
+
+  metas = doc.xpath('//meta')
+  meta_dict = {}
+  metas.each do |meta|
+    property = meta['property']
+    if property.nil?
+      # puts 'WARNING: Could not handle meta %s' % meta
+    else
+      content = meta['content']
+      if meta_dict.key?(property)
+        puts 'DEBUG: meta = %s' % meta
+        raise '%s already in meta_dict' % property
+      end
+      meta_dict[property] = content
+    end
+  end
+  # puts meta_dict
+  training['altid'] = meta_dict['og:url'].split('=')[-1]
+  training['type'] = meta_dict['og:title'].split(' ')[0]
+  training['date'] = meta_dict['og:title'].split(' ')[1]
+  training['description'] = meta_dict['og:description']
+
+  doc.xpath('//div').each do |div|
+    if div['class'] == 'TrainingProperty clrfix'
+      # puts "INFO: Handle trainingProperty %s" % div
+      div_dict = {}
+      div.xpath('//div').each do |part|
+        part_key = part['class']
+        if part_key == 'Key'
+          div_dict[part_key] = part.text
+        else
+          div_dict[part_key] = part
+        end
+      end
+      key = div_dict['Key']
+      if TRANSLATE_TP.key?(key)
+        if key == "Runda:"
+          href = div_dict['Value'].children[0]['href']
+          route_id = href.split('%3D')[-1]
+        else
+          training[TRANSLATE_TP[key]] = div_dict['Value'].text
+        end
+      else
+        puts "ERROR: Can't map training property %s" % div_dict['Key']
+      end
+    else
+      # puts "Don't know how to handle %s" % div
+    end
+  end
+
+  puts
+  puts "Training:"
+  pp training
+  if not route_id.nil?
+    puts "Route: %d" % route_id
+  end
+  puts
+end
+
 
 def import_fb_user(path)
   puts "About to parse " + path
@@ -30,9 +104,16 @@ def import_fb_user(path)
   else
     raise "Found several persons with id"
   end
+
+  # Now import trainings
+  trainings_path = path.split('/')[0..-3].join('/') + '/training/show.aspx?TrainingID=*'
+  Dir[trainings_path].each do |training_path|
+    import_fb_training(person_id, training_path)
+    # return # FIXME
+  end
 end
+
 
 ARGV.each do |path|
   import_fb_user(path)
 end
-
