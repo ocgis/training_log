373b898e1569273346e12af52ade0d07f5dfd7e1
Christer Gustavsson <cg@nocrew.org>
2019-12-08 07:28:49 +0100

    Added root route to people#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :people
+  get 'people/index'
+
+  get 'people/show'
+
+  root 'people#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
