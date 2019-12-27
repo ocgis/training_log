522d1f83098973cf2fce6cb1696b37df4f22b114
Christer Gustavsson <cg@nocrew.org>
2019-12-27 08:21:17 +0100

    Updated due to new rails version.

--- db/schema.rb
+++ db/schema.rb
@@ -12,48 +12,48 @@
 
 ActiveRecord::Schema.define(version: 20191215174345) do
 
-  create_table "intervals", force: :cascade do |t|
+  create_table "intervals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "duration_s"
     t.integer  "distance_m"
-    t.text     "comment"
+    t.text     "comment",     limit: 65535
     t.integer  "ix"
     t.integer  "training_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["training_id"], name: "index_intervals_on_training_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["training_id"], name: "index_intervals_on_training_id", using: :btree
   end
 
-  create_table "people", force: :cascade do |t|
+  create_table "people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.integer  "altid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "route_points", force: :cascade do |t|
-    t.float    "latitude"
-    t.float    "longitude"
+  create_table "route_points", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.float    "latitude",   limit: 24
+    t.float    "longitude",  limit: 24
     t.integer  "ix"
     t.integer  "route_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["route_id"], name: "index_route_points_on_route_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.index ["route_id"], name: "index_route_points_on_route_id", using: :btree
   end
 
-  create_table "routes", force: :cascade do |t|
-    t.float    "distance_km"
+  create_table "routes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.float    "distance_km", limit: 24
     t.string   "name"
     t.integer  "altid"
     t.integer  "person_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["person_id"], name: "index_routes_on_person_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["person_id"], name: "index_routes_on_person_id", using: :btree
   end
 
-  create_table "trainings", force: :cascade do |t|
+  create_table "trainings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "kind"
     t.datetime "date"
-    t.text     "description"
+    t.text     "description",   limit: 65535
     t.integer  "duration_s"
     t.integer  "distance_m"
     t.integer  "max_pulse_bpm"
@@ -61,12 +61,16 @@ ActiveRecord::Schema.define(version: 20191215174345) do
     t.integer  "energy_kcal"
     t.integer  "intensity"
     t.integer  "altid"
-    t.integer  "person_id"
     t.integer  "route_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["person_id"], name: "index_trainings_on_person_id"
-    t.index ["route_id"], name: "index_trainings_on_route_id"
+    t.integer  "person_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["person_id"], name: "index_trainings_on_person_id", using: :btree
+    t.index ["route_id"], name: "index_trainings_on_route_id", using: :btree
   end
 
+  add_foreign_key "intervals", "trainings"
+  add_foreign_key "route_points", "routes"
+  add_foreign_key "routes", "people"
+  add_foreign_key "trainings", "people"
 end
