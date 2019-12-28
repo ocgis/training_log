1c52dad9025cfe303119cfc675019d832da3f59a
Christer Gustavsson <cg@nocrew.org>
2019-12-28 19:57:12 +0100

    Implemented facebook authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :route_points
   resources :routes
   resources :intervals
