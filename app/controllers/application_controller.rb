5b43325c4dc7a474f1ff43f3a9a848359c16c42c
Christer Gustavsson <cg@nocrew.org>
2019-12-29 17:32:07 +0100

    Catch authorization exception and redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
+  end
+
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
   end
