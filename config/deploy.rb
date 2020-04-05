061d915b2140af491baab55a3e8093a29c7b0673
Christer Gustavsson <cg@nocrew.org>
2020-04-05 06:48:38 +0200

    Updated capistrano lock version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "~> 3.12.1"
 
 set :application, "training_log"
 set :repo_url, "git@github.com:ocgis/training_log.git"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -46,7 +46,7 @@ role :db,  %w{rails@127.0.0.1}
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    keys: %w(/home/user_name/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
