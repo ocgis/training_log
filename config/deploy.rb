aec0decac56de954d7da3ca6fe415670ff684801
Christer Gustavsson <cg@nocrew.org>
2020-04-05 06:57:17 +0200

    Changed secrets.yml into a linked file at deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :default_stage, "staging"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
