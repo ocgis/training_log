7337f3b06adb396984af5d9a612d66b64dd24922
Christer Gustavsson <cg@nocrew.org>
2022-07-19 07:14:28 +0200

    Fixed eslint issues.

--- app/javascript/components/EditTraining.jsx
+++ app/javascript/components/EditTraining.jsx
@@ -1,68 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import TrainingForm from "./TrainingForm";
-import TopMenu from "./TopMenu";
+import React from 'react';
+import PropTypes from 'prop-types';
+import TrainingForm from './TrainingForm';
+import TopMenu from './TopMenu';
+
+const afterSubmit = (response) => {
+  const training = response.data;
+  window.location.href = `/trainings/${training.id}`;
+};
 
 class EditTraining extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { training: null };
-        this.afterSubmit = this.afterSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { training: null };
+  }
 
-    componentDidMount() {
-        const {
-            match: {
-                params: { id }
-            }
-        } = this.props;
+  componentDidMount() {
+    const { history } = this.props;
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
 
-        const url = `/api/v1/trainings/${id}`;
+    const url = `/api/v1/trainings/${id}`;
 
-        fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("Network response was not ok.");
-            })
-            .then(response => {
-                this.setState({ training: response });
-            })
-            .catch(() => this.props.history.push("/"));
-    }
-
-    render() {
-        const { training } = this.state;
-        const {
-            match: {
-                params: { id }
-            }
-        } = this.props;
-
-        if (training !== null) {
-            return (
-                <div>
-                  <TopMenu />
-                  <TrainingForm training={training} afterSubmit={this.afterSubmit} />
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                  <TopMenu />
-                  <h1>Loading...</h1>
-                </div>
-            );
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         }
-    }
+        throw new Error('Network response was not ok.');
+      })
+      .then((response) => {
+        this.setState({ training: response });
+      })
+      .catch(() => history.push('/'));
+  }
 
+  render() {
+    const { training } = this.state;
 
-    afterSubmit(response) {
-        const training = response.data;
-        window.location.href = `/trainings/${training.id}`;
+    if (training !== null) {
+      return (
+        <div>
+          <TopMenu />
+          <TrainingForm training={training} afterSubmit={afterSubmit} />
+        </div>
+      );
     }
+    return (
+      <div>
+        <TopMenu />
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 }
-
+EditTraining.propTypes = {
+  match: PropTypes.shape().isRequired,
+  history: PropTypes.shape().isRequired,
+};
 
 export default EditTraining;
