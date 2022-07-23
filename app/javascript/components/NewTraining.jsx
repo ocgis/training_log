3051a77326d067a24476ea1abb0f143d18025f60
Christer Gustavsson <cg@nocrew.org>
2022-07-23 19:35:02 +0200

    Fixed eslint issues.

--- app/javascript/components/NewTraining.jsx
+++ app/javascript/components/NewTraining.jsx
@@ -1,31 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import TrainingForm from "./TrainingForm";
-import TopMenu from "./TopMenu";
+import React from 'react';
+import TrainingForm from './TrainingForm';
+import TopMenu from './TopMenu';
 
 class NewTraining extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { training: { intervals_attributes: [] } };
-        this.afterSubmit = this.afterSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { training: { intervals_attributes: [] } };
+    this.afterSubmit = this.afterSubmit.bind(this);
+  }
 
-    render() {
-        const { training } = this.state;
+  render() {
+    const afterSubmit = () => {
+      window.location.href = '/';
+    };
 
-        return (
-            <div>
-              <TopMenu />
-              <TrainingForm training={training} afterSubmit={this.afterSubmit} />
-            </div>
-        );
-    }
+    const { training } = this.state;
 
-
-    afterSubmit(resp) {
-        window.location.href = "/";
-    }
+    return (
+      <div>
+        <TopMenu />
+        <TrainingForm training={training} afterSubmit={afterSubmit} />
+      </div>
+    );
+  }
 }
-
 
 export default NewTraining;
