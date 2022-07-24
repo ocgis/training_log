f6ab289ffcf34ce52cd87b9a4ba7adfd82fe44bb
Christer Gustavsson <cg@nocrew.org>
2022-07-24 07:30:45 +0200

    Eslint fixes.

--- app/javascript/components/TopMenu.jsx
+++ app/javascript/components/TopMenu.jsx
@@ -1,63 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Menu } from "antd";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from "@ant-design/icons";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
 
-class TopMenu extends React.Component {
-    render() {
-        return (
-            <Menu mode="horizontal">
-              <Menu.Item>
-                <Link to={"/"}>
-                  Overview
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to={"/trainings/new"}>
-                  New training
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to={"/rawfiles"}>
-                  Uploaded files
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to={"/rawfiles/new"}>
-                  Upload file
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to={"/routes"}>
-                  Routes
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <a href={"/users/edit"}>
-                  Edit registration
-                </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a href={"/users/sign_out"} data-method="delete">
-                  Logout
-                </a>
-              </Menu.Item>
-            </Menu>
-        );
-    }
+function TopMenu() {
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item>
+        <Link to="/">
+          Overview
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/trainings/new">
+          New training
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/rawfiles">
+          Uploaded files
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/rawfiles/new">
+          Upload file
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/routes">
+          Routes
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="/users/edit">
+          Edit registration
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="/users/sign_out" data-method="delete">
+          Logout
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
 }
 
-class SubmitLink extends React.Component {
-    render () {
-        const { to } = this.props;
-    
-        return (<a href={to}>
-                {this.props.children}
-                </a>);
-    }
+function SubmitLink(props) {
+  const { children, to } = props;
+
+  return (
+    <a href={to}>
+      {children}
+    </a>
+  );
 }
+SubmitLink.propTypes = {
+  children: PropTypes.node.isRequired,
+  to: PropTypes.string.isRequired,
+};
 
 export default TopMenu;
