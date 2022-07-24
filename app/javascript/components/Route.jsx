2754d03050ff517dc7a1fc494a3c4671297b7874
Christer Gustavsson <cg@nocrew.org>
2022-07-24 23:11:00 +0200

    MapContainer -> Map.

--- app/javascript/components/Route.jsx
+++ app/javascript/components/Route.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {
-  MapContainer, Marker, Polyline, Popup, TileLayer,
+  Map, Marker, Polyline, Popup, TileLayer,
 } from 'react-leaflet';
 
 function Route(props) {
