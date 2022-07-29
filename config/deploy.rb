7f4f1c107c50d028066ed529ac18254223eed644
Christer Gustavsson <cg@nocrew.org>
2022-07-29 15:15:34 +0200

    Rubocop fixes. Lowered number of kept releases to 2.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock '~> 3.17.0'
 
-set :application, "training_log"
-set :repo_url, "git@github.com:ocgis/training_log.git"
+set :application, 'training_log'
+set :repo_url, 'git@github.com:ocgis/training_log.git'
 
-set :stages, ["staging", "production"]
-set :default_stage, "staging"
+set :stages, %w[staging production]
+set :default_stage, 'staging'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,20 +24,20 @@ set :default_stage, "staging"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'storage'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :default_env, { SECRET_KEY_BASE: "347M3" }
+set :default_env, { SECRET_KEY_BASE: '347M3' }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 2
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
