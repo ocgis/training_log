193697db20e3ae2e8e5db3e06be1786b1a833251
Christer Gustavsson <cg@nocrew.org>
2022-07-30 11:40:04 +0200

    Add turbo.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 import 'stylesheets/application';
+import '@hotwired/turbo-rails';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,6 +16,7 @@ import 'stylesheets/application';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
 
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
