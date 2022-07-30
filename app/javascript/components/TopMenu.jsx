2e4dcb6b67d0ce577635bdfc34e3ec2c58073a67
Christer Gustavsson <cg@nocrew.org>
2022-07-30 11:39:28 +0200

    Use button and turbo for login.

--- app/javascript/components/TopMenu.jsx
+++ app/javascript/components/TopMenu.jsx
@@ -1,48 +1,63 @@
+import axios from 'axios';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Menu } from 'antd';
+import { Button, Menu } from 'antd';
 
-function TopMenu() {
-  return (
-    <Menu mode="horizontal">
-      <Menu.Item>
-        <Link to="/">
-          Overview
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/trainings/new">
-          New training
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/rawfiles">
-          Uploaded files
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/rawfiles/new">
-          Upload file
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/routes">
-          Routes
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <a href="/users/edit">
-          Edit registration
-        </a>
-      </Menu.Item>
-      <Menu.Item>
-        <a href="/users/sign_out" data-method="delete">
-          Logout
-        </a>
-      </Menu.Item>
-    </Menu>
-  );
+class TopMenu extends React.Component {
+  render() {
+    return (
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link to="/">
+            Overview
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/trainings/new">
+            New training
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/rawfiles">
+            Uploaded files
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/rawfiles/new">
+            Upload file
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/routes">
+            Routes
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <a href="/users/edit">
+            Edit registration
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <Button
+            onClick={() => {
+              const csrfToken = document.querySelector('[name=csrf-token]').content;
+              axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
+              axios
+                .delete('/users/sign_out')
+                .then((response) => {
+                  this.ct_body = response.data;
+                });
+              alert('bah');
+            }}
+            style={{ border: 'none' }}
+          >
+            Logout
+          </Button>
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
 function SubmitLink(props) {
