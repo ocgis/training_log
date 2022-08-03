4776ae53852ad2f9f97300320377c7e0a292c45f
Christer Gustavsson <cg@nocrew.org>
2022-08-03 11:35:05 +0200

    Fixes and refactoring.

--- app/javascript/components/TopMenu.jsx
+++ app/javascript/components/TopMenu.jsx
@@ -1,74 +1,71 @@
-import axios from 'axios';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Button, Menu } from 'antd';
+import { Menu } from 'antd';
 
-class TopMenu extends React.Component {
-  render() {
-    return (
-      <Menu mode="horizontal">
-        <Menu.Item>
-          <Link to="/">
-            Overview
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/trainings/new">
-            New training
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/rawfiles">
-            Uploaded files
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/rawfiles/new">
-            Upload file
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/routes">
-            Routes
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <a href="/users/edit">
-            Edit registration
-          </a>
-        </Menu.Item>
-        <Menu.Item>
-          <form method="post" action="/users/sign_out">
-            <input type="hidden" name="_method" value="delete" />
-            <input type="hidden" name="authenticity_token" value={document.querySelector('[name=csrf-token]').content} />
-            <input
-              data-turbo="false"
-              type="submit"
-              value="Logout"
-              style={{
-                border: 'medium none',
-                'background-color': 'inherit',
-              }}
-            />
-          </form>
-        </Menu.Item>
-      </Menu>
-    );
-  }
+function TopMenu() {
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item key="overview">
+        <Link to="/">
+          Overview
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="newTraining">
+        <Link to="/trainings/new">
+          New training
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="uploadedFiles">
+        <Link to="/rawfiles">
+          Uploaded files
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="uploadFile">
+        <Link to="/rawfiles/new">
+          Upload file
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="routes">
+        <Link to="/routes">
+          Routes
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="editRegistration">
+        <a href="/users/edit">
+          Edit registration
+        </a>
+      </Menu.Item>
+      <Menu.Item key="logout">
+        <form method="post" action="/users/sign_out">
+          <input type="hidden" name="_method" value="delete" />
+          <input type="hidden" name="authenticity_token" value={document.querySelector('[name=csrf-token]').content} />
+          <input
+            data-turbo="false"
+            type="submit"
+            value="Logout"
+            style={{
+              border: 'medium none',
+              backgroundColor: 'inherit',
+            }}
+          />
+        </form>
+      </Menu.Item>
+    </Menu>
+  );
 }
 
 function SubmitLink(props) {
-  const { children, to } = props;
+  const { items, to } = props;
 
   return (
     <a href={to}>
-      {children}
+      {items}
     </a>
   );
 }
 SubmitLink.propTypes = {
-  children: PropTypes.node.isRequired,
+  items: PropTypes.node.isRequired,
   to: PropTypes.string.isRequired,
 };
 
